7e879751a21b7746ffba2fd7ef64cdb72772de66
nadinekhis <torontino123@gmail.com>
2020-10-19 20:45:55 +0300

    feat: implement basic functionality

--- momentum/script.js
+++ momentum/script.js
@@ -1,110 +1,136 @@
 // DOM Elements
-const time = document.querySelector('.time'),
-  greeting = document.querySelector('.greeting'),
-  name = document.querySelector('.name'),
-  focus = document.querySelector('.focus');
+const day = document.querySelector('.day'),
+    time = document.querySelector('.time'),
+    greeting = document.querySelector('.greeting'),
+    name = document.querySelector('.name'),
+    focus = document.querySelector('.focus');
 
 // Options
-const showAmPm = true;
 
-// Show Time
+
+// Show Day and Time
 function showTime() {
-  let today = new Date(),
-    hour = today.getHours(),
-    min = today.getMinutes(),
-    sec = today.getSeconds();
+    let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"]
 
-  // Set AM or PM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
+    let today = new Date(),
+        dayOfTheWeek = weekday[today.getDay()],
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
 
-  // 12hr Format
-  hour = hour % 12 || 12;
+    // Output Time
+    day.innerHTML = `${dayOfTheWeek}`
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+        sec
+    )} `;
 
-  // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
-    sec
-  )} ${showAmPm ? amPm : ''}`;
-
-  setTimeout(showTime, 1000);
+    setTimeout(showTime, 1000);
 }
 
 // Add Zeros
 function addZero(n) {
-  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
 // Set Background and Greeting
 function setBgGreet() {
-  let today = new Date(),
-    hour = today.getHours();
+    let today = new Date(),
+        hour = today.getHours();
 
-  if (hour < 12) {
-    // Morning
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-    greeting.textContent = 'Good Morning, ';
-  } else if (hour < 18) {
-    // Afternoon
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon, ';
-  } else {
-    // Evening
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/924T2Wv/night.jpg')";
-    greeting.textContent = 'Good Evening, ';
-    document.body.style.color = 'white';
-  }
+    if (hour < 6) {
+        // Night
+        document.body.style.backgroundImage =
+            "url(./assets/images/night/01.jpg)";
+        greeting.textContent = 'Good Night, ';
+        document.body.style.color = 'white';
+    }else if (hour < 12) {
+        // Morning
+        document.body.style.backgroundImage =
+            "url(./assets/images/morning/01.jpg)";
+        greeting.textContent = 'Good Morning, ';
+    } else if (hour < 18) {
+        // Afternoon
+        document.body.style.backgroundImage =
+            "url(./assets/images/day/01.jpg)";
+        greeting.textContent = 'Good Afternoon, ';
+    } else  {
+        // Evening
+        document.body.style.backgroundImage =
+            "url(./assets/images/evening/01.jpg)";
+        greeting.textContent = 'Good Evening, ';
+        document.body.style.color = 'white';
+    }
 }
 
 // Get Name
 function getName() {
-  if (localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Name]';
-  } else {
-    name.textContent = localStorage.getItem('name');
-  }
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
 }
 
 // Set Name
 function setName(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('name', e.target.innerText);
-      name.blur();
+    if (localStorage.getItem('name') === null) {
+        localStorage.setItem('name', e.target.innerText);
     }
-  } else {
-    localStorage.setItem('name', e.target.innerText);
-  }
+    if (e.type === 'keypress') {
+        // Make sure enter is pressed
+        if (e.which === 13 || e.keyCode === 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else if (e.type === 'click') {
+        e.target.innerText = '';
+    } else {
+        if (e.target.innerText === '') {
+            e.target.innerText = localStorage.getItem('name');
+        }
+        localStorage.setItem('name', e.target.innerText);
+    }
 }
 
 // Get Focus
 function getFocus() {
-  if (localStorage.getItem('focus') === null) {
-    focus.textContent = '[Enter Focus]';
-  } else {
-    focus.textContent = localStorage.getItem('focus');
-  }
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
 }
 
 // Set Focus
 function setFocus(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('focus', e.target.innerText);
-      focus.blur();
+    if (localStorage.getItem('focus') === null) {
+        localStorage.setItem('focus', e.target.innerText);
     }
-  } else {
-    localStorage.setItem('focus', e.target.innerText);
-  }
+    if (e.type === 'keypress') {
+        // Make sure enter is pressed
+        if (e.which === 13 || e.keyCode === 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            name.blur();
+        }
+    } else if (e.type === 'click') {
+        e.target.innerText = '';
+    } else {
+        if (e.target.innerText === '') {
+            e.target.innerText = localStorage.getItem('focus');
+        }
+        localStorage.setItem('focus', e.target.innerText);
+    }
 }
 
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
+name.addEventListener('click', setName);
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
+focus.addEventListener('click', setName);
+
+
 
 // Run
 showTime();
