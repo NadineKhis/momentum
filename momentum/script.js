a0f872fe2bb94a735ed956c6b5d7d66c9a4f605d
nadinekhis <torontino123@gmail.com>
2020-10-24 16:59:53 +0300

    refactor: change index.html location

--- momentum/script.js
+++ momentum/script.js
@@ -5,22 +5,70 @@ const day = document.querySelector('.day'),
     name = document.querySelector('.name'),
     focus = document.querySelector('.focus');
 
+
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg',
+    '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg',
+    '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+
+let i = 0;
+
+// let i = Math.floor(Math.random() * 21);
+
+function viewBgImage(data) {
+    const body = document.querySelector('body');
+    const src = data;
+    const img = document.createElement('img');
+    img.src = src;
+    img.onload = () => {
+        body.style.backgroundImage = `url(${src})`;
+    };
+}
+
+function getImage(base) {
+    const index = i % images.length;
+    const imageSrc = base + images[index];
+    viewBgImage(imageSrc);
+    i++;
+}
+
+function refreshImg() {
+
+}
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', refreshImg);
+
 // Options
+// function generatorBg() {
+//
+//     let i = Math.floor(Math.random() * 21);
+//     for () {}
+// }
 
-
-// Show Day and Time
-function showTime() {
+// Show Day
+function showDay() {
     let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday",
-        "Thursday", "Friday", "Saturday"]
+        "Thursday", "Friday", "Saturday"];
+    let months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"]
 
     let today = new Date(),
+        dateDay = today.getDate(),
         dayOfTheWeek = weekday[today.getDay()],
+        dayMonth = months[today.getMonth()];
+
+    day.innerHTML = `${dayOfTheWeek}<span>, </span>${dayMonth} ${dateDay}`;
+}
+
+// Show Time
+function showTime() {
+
+    let today = new Date(),
         hour = today.getHours(),
         min = today.getMinutes(),
         sec = today.getSeconds();
 
     // Output Time
-    day.innerHTML = `${dayOfTheWeek}`
     time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
         sec
     )} `;
@@ -41,25 +89,27 @@ function setBgGreet() {
     if (hour < 6) {
         // Night
         document.body.style.backgroundImage =
-            "url(./assets/images/night/01.jpg)";
+
+            "url(./momentum/assets/images/night/01.jpg)";
         greeting.textContent = 'Good Night, ';
         document.body.style.color = 'white';
-    }else if (hour < 12) {
+    } else if (hour < 12) {
         // Morning
         document.body.style.backgroundImage =
-            "url(./assets/images/morning/01.jpg)";
+            "url(./momentum/assets/images/morning/01.jpg)";
         greeting.textContent = 'Good Morning, ';
     } else if (hour < 18) {
         // Afternoon
         document.body.style.backgroundImage =
-            "url(./assets/images/day/01.jpg)";
+            "url(./momentum/assets/images/day/01.jpg)";
         greeting.textContent = 'Good Afternoon, ';
-    } else  {
+    } else {
         // Evening
+        getImage("./assets/images/evening/");
         document.body.style.backgroundImage =
-            "url(./assets/images/evening/01.jpg)";
+        "url(./momentum/assets/images/evening/01.jpg)";
         greeting.textContent = 'Good Evening, ';
-        document.body.style.color = 'white';
+        // document.body.style.color = 'white';
     }
 }
 
@@ -79,7 +129,8 @@ function setName(e) {
     }
     if (e.type === 'keypress') {
         // Make sure enter is pressed
-        if (e.which === 13 || e.keyCode === 13) {
+        // if (e.which === 13 || e.keyCode === 13) {
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
             localStorage.setItem('name', e.target.innerText);
             name.blur();
         }
@@ -109,7 +160,8 @@ function setFocus(e) {
     }
     if (e.type === 'keypress') {
         // Make sure enter is pressed
-        if (e.which === 13 || e.keyCode === 13) {
+        // if (e.which === 13 || e.keyCode === 13) {
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
             localStorage.setItem('focus', e.target.innerText);
             name.blur();
         }
@@ -131,8 +183,8 @@ focus.addEventListener('blur', setFocus);
 focus.addEventListener('click', setName);
 
 
-
 // Run
+showDay();
 showTime();
 setBgGreet();
 getName();
